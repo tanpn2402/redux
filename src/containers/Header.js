25da7a6cba4c1c2e7a40cf9a1701aa45ca447e3a
tanpn <tanpn2402@gmail.com>
2017-08-03 10:45:55 +0700

    merge with header branch v.2

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions/index';
+import React from 'react';
+import { Row, Col, Table, Panel,  } from 'react-bootstrap';
 
-class Header extends Component {
+const title = (
+  <h3>Welcome+AccountName+AccountID</h3>
+);
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-       
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-
-   
+export default class App extends React.Component {
+  render() {
+    return (
+        <Row className="show-grid" style={{border: "1px solid black"}}>
+          <Col xs={3} md={3}>
+            <img src={require('../assets/images/logoMAS.png')} />
+          </Col>
+          <Col xs={5} md={5}>
+            <Table striped bordered condensed hover>
+            <thead>
+             <tr>
+             <th>HSX | </th>
+             <th>HNX | </th>
+             <th>UPCOM | </th>
+             </tr>
+             </thead>
+             <tbody>
+             <tr>
+             <th>VN-INDEX 0 <img src={require('../assets/images/n.gif')}/> 0</th>
+             <th>HNX-INDEX 0 <img src={require('../assets/images/n.gif')}/> 0</th>
+             <th>UPCOM-INDEX 0 <img src={require('../assets/images/n.gif')}/> 0</th>
+             </tr>
+             <tr>
+             <th>TĂNG/GIẢM 0 <img src={require('../assets/images/u.gif')} /> 0 <img src={require('../assets/images/d.gif')} /> 0 <img src={require('../assets/images/n.gif')} /></th>
+             <th>TĂNG/GIẢM 0<img src={require('../assets/images/u.gif')} /> 0 <img src={require('../assets/images/d.gif')} /> 0 <img src={require('../assets/images/n.gif')} /></th>
+             <th>TĂNG/GIẢM 0 <img src={require('../assets/images/u.gif')} /> 0 <img src={require('../assets/images/d.gif')} /> 0 <img src={require('../assets/images/n.gif')} /></th>
+             </tr>
+             <tr>
+             <th>KLGD:0</th>
+             <th>KLGD:0</th>
+             <th>KLGD:0</th>
+             </tr>
+             </tbody>
+             </Table>
+          </Col>
+          <Col xs={4} md={4}>
+          <div>
+          <Panel header={title}>
+          Logout
+          </Panel>
+          </div>
+          </Col>
+        </Row>
+    );
+  }
 }
-
-const mapStateToProps = (state) => ({
-});
-
-const mapDispatchToProps = (dispatch, props) => ({
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
