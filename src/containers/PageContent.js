839941e3fd72a33b481af68f289ee0083dc0ba7f
phamnutuyetan <14520010@gm.uit.edu.vn>
2017-08-08 16:14:38 +0700

    Sua loi thieu file

---
+++ src/containers/PageContent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import BaseLayout from './BaseLayout.js';
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+
+class PageContent extends React.Component {
+    constructor () {
+        super()
+    }
+
+    render () {
+        console.log('PageContent', this.props)
+        return (
+            <div>
+                <BaseLayout 
+                    layout={this.props.mvLayout[this.props.mvPage]} 
+                    language= {this.props.language}
+                    /*onResize={this.onResize.bind(this)} 
+                    onResizeStart={this.onResizeStart.bind(this)}
+                    onResizeStop={this.onResizeStop.bind(this)}
+                    onDrag={this.onDrag.bind(this)} 
+                    onDragStart={this.onDragStart.bind(this)}
+                    onDragStop={this.onDragStop.bind(this)}
+                    onPinLayout={this.onPinLayout.bind(this)}
+                    onCloseLayout={this.onCloseLayout.bind(this)}*/
+                    >
+                </BaseLayout>
+            </div>
+        );
+    }
+
+    
+}
+
+const mapStateToProps = (state, props) => ({
+  mvLayout: state.menuSelected.tabList,
+  mvPage: state.menuSelected.page,
+  mvReload: state.menuSelected.reload
+  
+})
+
+const mapDispatchToProps = (dispatch, props) => ({
+    onChangeLayout: (menuid,pageid, tabList, reload) => {
+        dispatch(actions.menuRemoved(menuid, pageid, tabList, reload))
+      },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageContent)
