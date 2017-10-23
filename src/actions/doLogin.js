3a5d00f1193f9dab41b1e9de1c2fcd77cb6652c1
tanpn2402 <tanpn2402@gmail.com>
2017-10-23 17:17:01 +0700

    fix login ui

--- src/actions/doLogin.js
+++ src/actions/doLogin.js
@@ -4,87 +4,18 @@ import * as ACTION from '../api/action_name'
 import { browserHistory } from 'react-router';
 import { sessionService } from 'redux-react-session';
 import * as sessionApi from '../api/sessionApi';
-// import { browserHistory } from 'react-router';
-// import { sessionService } from 'redux-react-session';
-// import * as sessionApi from '../api/sessionApi';
-
-// const user = [
-//     {
-//         email: 'linh@yahoo.com',
-//         name: 'Jordan Walke',
-//         clientID: 'linh',
-//         password: '123'
-//     },
-//     {
-//         email: 'aaa@yahoo.com',
-//         name: 'Dan Abramov, Andrew Clark',
-//         clientID: 'norahshy',
-//         password: '123'
-//     }
-// ];
-
-// export function doLogin(username, password) {
-
-//     return dispatch => {
-//         authorize(username, password, error => {
-//             if (!error) {
-//                 sessionApi.login(user[0]).then(response => {
-//                     const { token, data } = response;
-//                     sessionService.saveSession({ token })
-//                         .then(() => {
-//                             sessionService.saveUser(data)
-//                                 .then(() => {
-//                                     browserHistory.replace('/');
-//                                 });
-//                         });
-//                 });
-//             } else {
-//                 console.log('Everything is not okay');
-//                 dispatch(setLoginError(true));
-//             }
-//         });
-//     }
-// }
-
-// export function setLoginError(isLoginError) {
-//     return {
-//         type: ActionTypes.SET_LOGIN_ERROR,
-//         isLoginError
-//     }
-// }
-
-// function authorize(username, password, callback) {
-//     setTimeout(() => {
-//         if (username === user[0].clientID && password === user[0].password) {
-//             return callback(null);
-//         } else {
-//             return callback('Something Wrong');
-//         }
-//     }, 10);
-// }
-
-// export const logout = () =>
-//     () =>
-//         sessionApi.logout().then(() => {
-//             sessionService.deleteSession();
-//             sessionService.deleteUser();
-//             browserHistory.replace('/login');
-//         }).catch(err => {
-//             throw (err);
-//         });
-
 
 export function doLogin(params) {
     return (dispatch) => {
-        return api.login(ACTION.DOLOGIN, params, dispatch, isLogin)
-    }
-}
-
-export function isLogin(response) {
-    let isLoginError = !response.success
-    return {
-        type: ActionTypes.SET_LOGIN_ERROR,
-        isLoginError: isLoginError
+        return api.login(ACTION.DOLOGIN, params, dispatch, function(response){
+            let isLoginError = !response.success
+            console.log(response)
+            return {
+                type: ActionTypes.DOLOGINACTION,
+                result: response,
+                mvClientID: params.mvClientID
+            }
+        })
     }
 }
 
