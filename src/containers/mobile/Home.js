58a78113d4cd364a0bbeaae9cc854ae92309d4ea
Dao Tuan <heryhudson@gmail.com>
2017-11-13 10:58:06 +0700

    overwrite light theme

--- src/containers/mobile/Home.js
+++ src/containers/mobile/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
     }
 
     render() {
-        let theme = getTheme('red')
+        let theme = getTheme(config.cache.theme)
         let lang = getLanguage(config.cache.lang)
 
         return (
